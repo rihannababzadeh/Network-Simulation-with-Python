d2cc63403d9c6e9fc724bb9a7391fdc1b8db2f70
unknown <phoenix.softeng@gmail.com>
2023-09-20 19:12:16 -0600

    Cleaning the Slot class

--- ns/port/slot.py
+++ ns/port/slot.py
@@ -1,47 +1,37 @@
-import simpy
+"""
+Slot Class
+This class represents a slotted system in a network simulation. Each slot has a fixed duration
+and can potentially contain packets waiting for transmission. 
+Each sourse has its own slot instance. It schedules the transmission of packets based on the specified rate and packet size.
+Packets are transmitted one by one from a slot to the connected medium (Wire).
+"""
 
 class Slot:
-    instances = []  # Class-level attribute to store references to all instances
-    # Class-level variable to keep track of the condition
-    at_least_two_slots_with_packets = False
-
     def __init__(self, env, rate, packet_size):
         self.env = env
         self.rate = rate
         self.packet_size = packet_size
         self.packets_in_slot = []
         self.out = None  # Add out attribute to connect the Slot to the wire
-
-        # Add the current instance to the class-level instances list
-        self.instances.append(self)
-
-        # Start the slot scheduling process
-        self.env.process(self.schedule_slots())
+        self.env.process(self.schedule_slots()) # Start the slot scheduling process
 
     def schedule_slots(self):
+        # Schedule time slots with a fixed duration. This method controls the transmission
+        # of packets in each slot
         print("initial delay in Slot: ",0)
         yield self.env.timeout(0) 
         slot_duration = 1.5 #1.0 / self.rate # # 
-        # print("Slot duration is: ", slot_duration)
         while True:
             self.current_slot_start = self.env.now
             print("current slot time: ", self.env.now)
             self.current_slot_end = self.current_slot_start + slot_duration
             yield self.env.timeout(slot_duration)
             print("now is after yielding for slot duration: ", self.env.now)
-            # Check if at least two slots have at least one packet queued
-            # if sum(slot.has_packets_queued() for slot in self.instances) >= 2:
-            #     Slot.at_least_two_slots_with_packets = True
-            #     print("At least two slots have at least one packet queued:")
-            #     for slot in self.instances:
-            #         if slot.has_packets_queued():
-            #             print(f"Slot instance {Slot.instances.index(slot)} - Packet IDs: {[packet.packet_id for packet in slot.packets_in_slot]}")
 
             # Transmit one packet from the slot if it's not empty
-            # So here we send only one packet to the wire as we are in one instance of slot.
+            # So here we send only one packet to the wire as we are in one instance of slot each time
             if self.packets_in_slot:
                 packet = self.packets_in_slot.pop(0) 
-
                 if self.rate > 0:
                     packet.begin_transmission = self.env.now  # Record the begin_transmission time
                     print("Begins slot transmission at: {:.3f}, Packet ID: {}, flow ID: {}".format(packet.begin_transmission, packet.packet_id, packet.flow_id))
@@ -50,17 +40,9 @@ class Slot:
                     # transmission_time = 1.5 # packet.size * 8.0 / self.rate
                     # yield self.env.timeout(transmission_time)
                     print("Ends slot transmission at: {} , Packet ID: {}, flow ID: {}".format(self.env.now, packet.packet_id, packet.flow_id))
-
                 self.out.put(packet)  # Send packet to the out attribute (wire)
 
     def put(self, packet):
         # If a packet arrives, it will be queued
         print("Adding the packet with id: {} with flow_id: {} to the slot buffer at time {}.".format(packet.packet_id, packet.flow_id, self.env.now))
         self.packets_in_slot.append(packet)
-
-    def has_packets_queued(self):
-        return len(self.packets_in_slot) >= 1 # Is there a packet queued in this slot?
-    
-    @staticmethod
-    def has_at_least_two_slots_with_packets():
-        return Slot.at_least_two_slots_with_packets
